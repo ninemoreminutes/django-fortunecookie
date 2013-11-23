8ba50a90ad23b2cefa6c667a7edddf8526fd27e2
chris <chris@b5bb89e0-60e0-409f-8dd1-51e0510b34c3>
2013-11-23 06:07:54 +0000

    Update test site settings.
    git-svn-id: https://projects.ninemoreminutes.com/svn/django-fortunecookie/trunk@14 b5bb89e0-60e0-409f-8dd1-51e0510b34c3

--- test_site/site_settings.py
+++ test_site/site_settings.py
@@ -14,3 +14,7 @@ DATABASES = {
 MEDIA_ROOT = os.path.join(ROOT_PATH, 'public', 'media')
 
 STATIC_ROOT = os.path.join(ROOT_PATH, 'public', 'static')
+
+ALLOWED_HOSTS = ['*']
+
+SECRET_KEY = 'a9f93bfdd9a1c66039920623b3b90c74d8aa4b4f'
