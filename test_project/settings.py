287a4be4d1b75cdc52761c76fac65873a63f7e61
chris <chris@b5bb89e0-60e0-409f-8dd1-51e0510b34c3>
2017-05-22 19:37:52 +0000

    Fix flake8 errors in test_project.
    git-svn-id: https://projects.ninemoreminutes.com/svn/django-fortunecookie/trunk@30 b5bb89e0-60e0-409f-8dd1-51e0510b34c3

--- test_project/settings.py
+++ test_project/settings.py
@@ -6,6 +6,9 @@ import sys
 import django
 from django.conf import global_settings
 
+# DJ Database URL
+import dj_database_url
+
 # Update this module's local settings from the global settings module.
 this_module = sys.modules[__name__]
 for setting in dir(global_settings):
@@ -23,9 +26,6 @@ DATABASES = {
         'NAME': os.path.join(PROJECT_ROOT, 'test_project.sqlite3'),
     }
 }
-
-
-import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
--- test_project/wsgi.py
+++ test_project/wsgi.py
@@ -4,10 +4,10 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_project.settings')
 
 # Django
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application  # noqa
 
 # Whitenoise
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise.django import DjangoWhiteNoise  # noqa
 
 
 application = get_wsgi_application()
